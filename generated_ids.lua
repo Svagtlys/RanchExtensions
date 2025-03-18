1e489b600ed2197fd3d6c4086b73310128c86812
Momo the Bestest <45446348+Svagtlys@users.noreply.github.com>
2025-03-18 08:25:22 -0500

    Initial commit. Got the basics working, but for some reason the milking barn doesn't add more workplaces to the dairy farm, despite the tech unlock saying it will.

---
+++ generated_ids.lua
@@ -0,0 +1,13 @@
+-- This is a generated file
+-- Do not modify it, unless you know exactly what you are doing
+
+ModId = "e757cfe6-cb25-49b5-91e1-6cbfbde5f163"
+
+IdMap = {
+	{ "BUILDING_PART_DAIRY_FARM_DAIRY_BARN", "BUILDING_PART", "3db1db4a-e1f7-4230-8f82-5ce6273e023f" },
+	{ "UNLOCKABLE_DAIRY_FARMING_IMPROVEMENT", "UNLOCKABLE", "a60a0d87-3c97-4c26-84e4-fcab8d78303b" },
+	{ "BUILDING_FUNCTION_DAIRY_FARM_DAIRY_BARN", "BUILDING_FUNCTION", "d450e5f7-c8bb-4ddc-bb99-bc8b958b3ae5" },
+	{ "BUILDING_FUNCTION_DAIRY_FARM_MILKING_BARN", "BUILDING_FUNCTION", "3d7e0b50-8292-4442-b1c2-6678b62db6c7" },
+	{ "models/dairy.fbx", "", "983c0e46-7755-4f57-ba75-4883e1d1dc97" },
+	{ "BUILDING_PART_DAIRY_FARM_MILKING_BARN", "BUILDING_PART", "1ba3847d-a82e-4d19-946a-49124d0e4979" },
+}
